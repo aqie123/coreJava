f0cae96ee44c5fdf5b80c444d16371c5470bd6bb
aqie123 <2924811900@qq.com>
2017-12-11 19:52:05 +0800

    day19

---
+++ aqie1/Demo.java
@@ -0,0 +1,14 @@
+package aqie1;
+
+public class Demo {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.method();
+    }
+}
+
+class Test{
+    void method(){
+
+    }
+}
---
+++ aqie1/Demo1.java
@@ -0,0 +1,14 @@
+package aqie1;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        t.method();
+    }
+}
+
+class Test1{
+    void method(){
+
+    }
+}
