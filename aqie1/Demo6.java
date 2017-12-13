66979b9fcc4d545ad8f83d9c8e92d2527fd08621
aqie123 <2924811900@qq.com>
2017-12-13 19:11:36 +0800

    day21

---
+++ aqie1/Demo6.java
@@ -0,0 +1,20 @@
+package aqie1;
+
+public class Demo6 {
+    public static void main(String[] args) {
+        Test6 t = new Test6();
+        t.method();
+    }
+}
+
+class Test6{
+    // BufferedReader 功能
+    void method(){
+
+    }
+}
+
+// 带行号的缓冲类
+class BufferedLineNum{
+
+}
